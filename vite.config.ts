336abd2db41251d13fd421b9b8e05aa5d3702296
Boris Lepikhin <boris@lepikhin.com>
2022-09-01 11:20:26 -0700

    v0.1.13

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import { resolve } from "path"
 import { defineConfig } from "vite"
-const path = require("path")
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "Momentum Modal",
       fileName: `momentum-modal`,
     },
