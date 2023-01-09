1c610a41eb42f94d7cb981db7ab382aaf1ee222f
Jeffrey van Hees <jeffrey@emeals.nl>
2023-01-09 19:50:12 +0100

    Update use-modal.ts

--- src/use-modal.ts
+++ src/use-modal.ts
@@ -1,5 +1,5 @@
 import { usePage } from "@inertiajs/vue3"
-import { router } from '@inertiajs/vue3'
+import { router } from "@inertiajs/vue3"
 import { defineAsyncComponent, h, nextTick, watch, computed, ref, shallowRef } from "vue"
 import axios from "axios"
 import resolver from "./resolver"
