d26566f77faeb46ca40ad24912414dd946595d90
Boris Lepikhin <boris@lepikhin.com>
2023-02-14 23:23:48 -0800

    inertia v1.0.1 fix

--- src/use-modal.ts
+++ src/use-modal.ts
@@ -4,7 +4,7 @@ import { defineAsyncComponent, h, nextTick, watch, computed, ref, shallowRef } f
 import axios from "axios"
 import resolver from "./resolver"
 
-const modal = computed(() => usePage().props?.modal)
+const modal = computed(() => usePage()?.props?.modal)
 const props = computed(() => modal.value?.props)
 const key = computed(() => modal.value?.key)
 
